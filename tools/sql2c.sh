0c27f22586e1460bed0046d070a601cbca39ad5b
Marcus Rohrmoser <mrohrmoser@acm.org>
2014-06-21 10:38:26 +0200

    bashism—-

--- tools/sql2c.sh
+++ tools/sql2c.sh
@@ -28,7 +28,7 @@ echo "\tconst char *$(basename "$1" .sql)_sql = // generated via $0 $@"
 # turn comments into whitespace
 sed -e 's/^--.*$//' "$1" | while IFS= read sql # http://stackoverflow.com/a/7879523
 do
-  if [ "$sql" == "" ] ; then continue ; fi  # ignore blank lines
+  if [ "$sql" = "" ] ; then continue ; fi  # ignore blank lines
   # should escape ", but I don't care for now.
   echo "\t\"$sql\" \"\\\n\" \\"
 done
