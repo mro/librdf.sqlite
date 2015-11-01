7f848229855bce49a87409370370ea513698bf81
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-11-01 18:01:29 +0100

    verbose gcc linker. refs #13

--- test/run.sh
+++ test/run.sh
@@ -5,10 +5,10 @@ set -x
 TMP=tmp
 rm "$TMP"/*
 
-gcc -v -pedantic -g -O0 -std=c99 -D DEBUG=1 -I /usr/include/raptor2 -I /usr/include/rasqal -c -o "$TMP"/loader.o loader.c && {
-	echo "##########################################################################"
-	gcc -v -o "$TMP"/loader -lrdf -lsqlite3 "$TMP"/loader.o && {
-		# valgrind --leak-check=full --show-reachable=yes \
-		"$TMP"/loader "file://$(pwd)/loader.ttl" "http://purl.mro.name/rdf/sqlite/" 2> /dev/null
-	}
+gcc -pedantic -g -O0 -std=c99 -D DEBUG=1 -I /usr/include/raptor2 -I /usr/include/rasqal -c -o "$TMP"/loader.o loader.c && {
+  echo "##########################################################################"
+  gcc -Wl,--verbose -o "$TMP"/loader -lrdf -lsqlite3 "$TMP"/loader.o && {
+    # valgrind --leak-check=full --show-reachable=yes \
+    "$TMP"/loader "file://$(pwd)/loader.ttl" "http://purl.mro.name/rdf/sqlite/" 2> /dev/null
+  }
 }
