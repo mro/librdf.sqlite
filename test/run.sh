48b9ac28b5279ad69517cbe26e6006092bbb353b
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-11-01 17:58:12 +0100

    verbose gcc. refs #13

--- test/run.sh
+++ test/run.sh
@@ -5,8 +5,9 @@ set -x
 TMP=tmp
 rm "$TMP"/*
 
-gcc -pedantic -g -O0 -std=c99 -D DEBUG=1 -I /usr/include/raptor2 -I /usr/include/rasqal -c -o "$TMP"/loader.o loader.c && {
-	gcc -o "$TMP"/loader -lrdf -lsqlite3 "$TMP"/loader.o && {
+gcc -v -pedantic -g -O0 -std=c99 -D DEBUG=1 -I /usr/include/raptor2 -I /usr/include/rasqal -c -o "$TMP"/loader.o loader.c && {
+	echo "##########################################################################"
+	gcc -v -o "$TMP"/loader -lrdf -lsqlite3 "$TMP"/loader.o && {
 		# valgrind --leak-check=full --show-reachable=yes \
 		"$TMP"/loader "file://$(pwd)/loader.ttl" "http://purl.mro.name/rdf/sqlite/" 2> /dev/null
 	}
