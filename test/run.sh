b10505ed223a0b7493b982bcd0a8dbf6e9b9d87d
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-11-01 22:28:08 +0100

    put gcc linker options and —lrdf lsqlite3 last. refs #13

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,8 @@ rm "$TMP"/*
 
 gcc -pedantic -g -O0 -std=c99 -D DEBUG=1 -I /usr/include/raptor2 -I /usr/include/rasqal -c -o "$TMP"/loader.o loader.c && {
   echo "##########################################################################"
-  gcc -Wl,--verbose -o "$TMP"/loader -lrdf -lsqlite3 "$TMP"/loader.o && {
+  # http://ubuntuforums.org/showthread.php?t=1936253&p=11742200#post11742200
+  gcc -o "$TMP"/loader "$TMP"/loader.o -lrdf -lsqlite3 && {
     # valgrind --leak-check=full --show-reachable=yes \
     "$TMP"/loader "file://$(pwd)/loader.ttl" "http://purl.mro.name/rdf/sqlite/" 2> /dev/null
   }
