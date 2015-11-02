e689cf6732b66c68dcfc457877d88beae269c2f2
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-11-02 09:14:54 +0100

    travis, give clang a try. refs #13

--- test/run.sh
+++ test/run.sh
@@ -32,10 +32,10 @@ run_test() {
   echo -n "Running $test_name "
 
   rm "$TMP"/*
-  gcc -pedantic -g -O0 -std=c99 -D DEBUG=1 -I "/usr/include/raptor2" -I "/usr/include/rasqal" -c -o "$TMP/rdf_storage_sqlite_mro.o" "../rdf_storage_sqlite_mro.c" && {
-    gcc -pedantic -g -O0 -std=c99 -D DEBUG=1 -I "/usr/include/raptor2" -I "/usr/include/rasqal" -c -o "$TMP/$test_name".o "$test_src" && {
+  $CC -pedantic -g -O0 -std=c99 -D DEBUG=1 -I "/usr/include/raptor2" -I "/usr/include/rasqal" -c -o "$TMP/rdf_storage_sqlite_mro.o" "../rdf_storage_sqlite_mro.c" && {
+    $CC -pedantic -g -O0 -std=c99 -D DEBUG=1 -I "/usr/include/raptor2" -I "/usr/include/rasqal" -c -o "$TMP/$test_name".o "$test_src" && {
       # http://ubuntuforums.org/showthread.php?t=1936253&p=11742200#post11742200
-      gcc -o "$TMP/a.out" "$TMP/rdf_storage_sqlite_mro.o" "$TMP/$test_name".o -lrdf -lsqlite3 && {
+      $CC -o "$TMP/a.out" "$TMP/rdf_storage_sqlite_mro.o" "$TMP/$test_name".o -lrdf -lsqlite3 && {
         # valgrind --leak-check=full --show-reachable=yes \
         "$TMP/a.out" "file://$(pwd)/$test_base.ttl" "http://purl.mro.name/rdf/sqlite/" 2> /dev/null
         code=$?
