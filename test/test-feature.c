b1d9c7486a5d64299f159a938b8ac9395715a07f
Marcus Rohrmoser <github@mro.name>
2015-11-03 22:47:13 +0100

    travis log 💄. refs #13

--- test/test-feature.c
+++ test/test-feature.c
@@ -37,7 +37,7 @@
     char *message = test(); tests_run++; \
     const long long unsigned int t_finish = 7.5e8 + clock() * 1.0e9 / CLOCKS_PER_SEC; \
     printf("travis_time:end:%s_time:start=%llu,finish=%llu,duration=%llu\n", marker, t_start, t_finish, t_finish-t_start); \
-    printf("travis_fold:end:%s\n", marker); \
+    printf("travis_fold:end:%s\r", marker); \
     printf("%s%s %s %s\n", message ? (ANSI_COLOR_F_RED "✗") : (ANSI_COLOR_F_GREEN "✓"), ANSI_COLOR_RESET, marker, message ? message : ""); \
     if( message ) return message; \
 } while( 0 )
